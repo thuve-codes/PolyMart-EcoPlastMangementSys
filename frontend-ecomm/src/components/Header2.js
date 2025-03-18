cc1926b2b2c8df4730d8fb47bf0f22ae175f96b5
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-18 19:17:01 +0530

    ADDED some new features to the header

--- frontend-ecomm/src/components/Header2.js
+++ frontend-ecomm/src/components/Header2.js
@@ -7,8 +7,8 @@ export default function Header2() {
         {/* Container for Navbar */}
         <div className="container-fluid">
           {/* Brand Logo */}
-          <a className="navbar-brand" href="#">
-            <img src={polymartLogo} alt="Polymart Logo" className="logo" /> Polymart EStore
+          <a className="navbar-brand navname" href="#">
+            <img src={polymartLogo} alt="Polymart Logo" className="logo" /> Poly EStore
           </a>
 
           {/* Navbar Toggle Button for Mobile */}
@@ -29,15 +29,15 @@ export default function Header2() {
             
             {/* Search Bar (Centered) */}
             <form className="d-flex search-form mx-auto">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <input className="form-control me-2 searchbar" type="search" placeholder="Enter here..." aria-label="Search" />
+              <button className="btn btn-outline-success searchbtn" type="submit">Search</button>
             </form>
 
             {/* Navbar Menu (Right Aligned) */}
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navbar">
               <li className="nav-item dropdown">
                 <a
-                  className="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle navname"
                   href="#"
                   role="button"
                   data-bs-toggle="dropdown"
@@ -46,29 +46,29 @@ export default function Header2() {
                   Home
                 </a>
                 <ul className="dropdown-menu dropdown-menu-end"> {/* Aligns to the right */}
-                  <li><a className="dropdown-item" href="#">Dashboard</a></li>
-                  <li><a className="dropdown-item" href="#">Profile</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Settings</a></li>
+                  <li><a className="dropdown-item navname" href="#" >Dashboard</a></li>
+                  <li><a className="dropdown-item navname" href="#">Profile</a></li>
+                  <li><hr className="dropdown-divider navname" /></li>
+                  <li><a className="dropdown-item navname" href="#">Settings</a></li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link navname" href="#">Link</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link navname" href="#">Contact</a>
               </li>
             </ul>
           </div>
           <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <button className="btn" id="login_btn">Login</button>
+                    <button className="btn navname" id="login_btn">Login</button>
                     
-                    <span id="cart" className="ml-3">Cart
+                    <span id="cart" className="ml-3 navname">Cart
                     {
                    //<img src={cart} alt="Polymart Logo" width={"35px"}/> 
                     }
                    </span>
-                    <span className="ml-1" id="cart_count">2</span>
+                    <span className="ml-1 navname" id="cart_count">2</span>
                 </div>
         </div>
       </nav>
