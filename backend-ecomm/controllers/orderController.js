92d01e260c766ff7c31ca4cd3bb438e2418b38bf
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-20 15:15:21 +0530

    Backend Finished

--- backend-ecomm/controllers/orderController.js
+++ backend-ecomm/controllers/orderController.js
@@ -1,9 +1,24 @@
 const orderModel = require('../models/orderModel');
-exports.createOrder =(req,res, next) => {
-    console.log(req.body, 'DATA');
+
+//Create order- api/v1/order
+exports.createOrder =async(req,res, next) => {
+    //console.log(req.body, 'DATA');
+    const cartItems = req.body;
+    const amount=Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
+    const status='Processing';
+
+    const order = await orderModel.create({
+        cartItems,
+        amount,
+        status,
+        createdAt: Date.now()
+    })
+
+    console.log(amount, 'AMOUNT');
     
     res.json({
         success: true,
-        message: 'Create order working!'
+        order
+      //  message: 'Create order working!'
     })  
 }
