1a461f614b629dc489bb73ae444d60bc353b5ed8
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-04-25 12:11:36 +0530

    Add MyOrder, Updated nav bar searchbar wth bend

--- backend-ecomm/routes/order.js
+++ backend-ecomm/routes/order.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createOrder, getAllOrders } = require('../controllers/orderController');
+const { createOrder, getAllOrders, getOrderById } = require('../controllers/orderController');
 const router = express.Router();
 
 router.route('/order')
   .post(createOrder)
   .get(getAllOrders);
+
+
 
 module.exports = router;
--- frontend-ecomm/src/App.js
+++ frontend-ecomm/src/App.js
@@ -4,38 +4,38 @@ import Header2 from './components/Header2';
 import Home from './pages/Home';
 import Chatpage from './pages/chatpage';
 import { useState } from 'react';
-
-import { BrowserRouter , Router,Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductDetail from './pages/ProductDetail';
-
-import {ToastContainer} from 'react-toastify';
+import MyOrders from './pages/MyOrder'; // Changed variable name to start with capital letter
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Cart from './pages/Cart';
 
-
 function App() {
-
   const [cartItems, setCartItems] = useState([]);
 
   return (
     <div className="App">
       <BrowserRouter>
-      <ToastContainer position='top-center' theme='dark'/>
-      <Header2 cartItems={cartItems}/>
-        <div>
-        <Routes>
-        <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems}/>} />
-          
-          <Route path="/" element={<Chatpage />} />
-          <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems}/>} />
-        </Routes>
-        </div>
+        <ToastContainer position='top-center' theme='dark' />
+        <Header2 cartItems={cartItems} />
+        
+        <main> {/* Better semantic HTML */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Home />} />
+            <Route 
+              path="/product/:id" 
+              element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems} />} 
+            />
+            <Route path="/chat" element={<Chatpage />} /> {/* Fixed path and element */}
+            <Route path="/orders" element={<MyOrders />} /> {/* Fixed variable name and path */}
+            <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
+          </Routes>
+        </main>
         
         <Footer />
-
-        </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
