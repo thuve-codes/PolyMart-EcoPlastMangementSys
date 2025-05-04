79ff0b2fcd66916c30da430013daee96c2efe38d
thuve-codes <thuverakantharma@gmail.com>
2025-05-04 23:46:08 +0530

    images renamed

--- frontend-ecomm/src/components/ProductCard.js
+++ frontend-ecomm/src/components/ProductCard.js
@@ -5,11 +5,12 @@ export default function ProductCard({product}) {
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
         <div className="card p-3 rounded">
-          <img
-            className="card-img-top mx-auto"
-            src={product.images[0].image}
-            alt={product?.name}
-          />
+        <img
+          className="card-img-top mx-auto"
+          src={product?.images?.[0]?.image || "/default-image.png"}
+          alt={product?.name || "Product"}
+        />
+
           <div className="card-body d-flex flex-column">
             <h5 className="card-title">
               <Link to={"/product/"+product._id} >{product.name}</Link>
